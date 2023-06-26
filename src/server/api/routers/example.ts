90819f4686d736a4a834b1072dd815465de40b0d
Wesley Davenport <daven.wesley@gmail.com>
2023-06-26 17:37:35 -0400

    Next image tag, stlye up

--- src/server/api/routers/example.ts
+++
@@ -1,9 +0,0 @@
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-
-import { z } from "zod";
-
-export const exampleRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
-  }),
-});
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,46 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+import { TRPCError } from "@trpc/server";
+import { User } from "@clerk/nextjs/dist/types/server";
+import { clerkClient } from "@clerk/nextjs";
 import { z } from "zod";
 
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.imageUrl,
+  };
+};
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const post = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: post.map((p) => p.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    return post.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+      if (!author || !author.username)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
