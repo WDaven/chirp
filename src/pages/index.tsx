c837588949d6e23407b086d671c1957feab2ffc7
Wesley Davenport <daven.wesley@gmail.com>
2023-06-26 21:34:42 -0400

    New Files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { useState } from "react";
 import { type NextPage } from "next/types";
 import toast from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime);
 
@@ -84,10 +85,16 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex gap-1  text-slate-300">
-          <span>{`@${author.username} `} </span>
-          <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          <Link href={`/@${author.username}`}>
+            <span>{`@${author.username} `} </span>
+          </Link>
+          <Link href={`/post/${post.id}`}>
+            <span>{` · ${dayjs(post.createdAt).fromNow()}`}</span>
+          </Link>
         </div>
-        <span className="text-2xl">{post.content}</span>
+        <Link href={`/post/${post.id}`}>
+          <span className="text-2xl">{post.content}</span>
+        </Link>
       </div>
     </div>
   );
